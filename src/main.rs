24445f03aed745b876233ea4041c66905ae3a8cf
Eduardo Ferreira <eduardo.sni.ferreira@gmail.com>
2023-04-03 14:04:24 -0300

    Send commands to docker and retrieve response

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
+use bollard::container::{Config, RemoveContainerOptions};
+use bollard::service::{ContainerCreateResponse, CreateImageInfo, HostConfig, Mount};
+use bollard::Docker;
 use std::error::Error;
 use tokio::signal;
 use warp::{http, Filter};
-use bollard::container::{Config, RemoveContainerOptions};
-use bollard::service::{HostConfig, Mount, CreateImageInfo, ContainerCreateResponse};
-use bollard::{Docker};
 
 use bollard::exec::{CreateExecOptions, StartExecResults};
 use bollard::image::CreateImageOptions;
 use futures_util::{stream::StreamExt, TryStreamExt};
+use json;
 
 const IMAGE: &str = "ultrafunk/undetected-chromedriver:latest";
 
@@ -18,24 +19,28 @@ async fn main() {
     let response = create_container(docker.clone()).await.unwrap();
     let _ = docker.start_container::<String>(&response.id, None).await;
 
-    let vizer_home = warp::get()
-        .and(warp::path("vizer")
-        .and(warp::path("v1"))
-        .and(warp::path("home"))
-        .and(warp::path::end())
-        .and_then(move || {home_page(docker.clone(), response.id.clone())}));
+    let vizer_home = warp::get().and(
+        warp::path("vizer")
+            .and(warp::path("v1"))
+            .and(warp::path("home"))
+            .and(warp::path::end())
+            .and_then(move || home_page(docker.clone(), response.id.clone())),
+    );
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
 
-    let routes = vizer_home.or(hello);    
+    let routes = vizer_home.or(hello);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
 async fn home_page(docker: Docker, id: String) -> Result<impl warp::Reply, warp::Rejection> {
     let response = exec(docker, &id).await;
-    Ok(warp::reply::with_status(response, http::StatusCode::OK))
+    Ok(warp::reply::with_status(
+        response.replace("'", "\""),
+        http::StatusCode::OK,
+    ))
 }
 
 async fn create_image(docker: Docker) {
@@ -52,10 +57,11 @@ async fn create_image(docker: Docker) {
         .await;
 }
 
-
-async fn create_container(docker: Docker) -> Result<ContainerCreateResponse, bollard::errors::Error>{
+async fn create_container(
+    docker: Docker,
+) -> Result<ContainerCreateResponse, bollard::errors::Error> {
     let mut mounts = Vec::<Mount>::new();
-    mounts.push(Mount{
+    mounts.push(Mount {
         target: Some("/data".to_string()),
         source: Some("scrapper-rust".to_string()),
         typ: Some(bollard::service::MountTypeEnum::VOLUME),
@@ -66,7 +72,7 @@ async fn create_container(docker: Docker) -> Result<ContainerCreateResponse, bol
         image: Some(IMAGE),
         tty: Some(true),
         entrypoint: Some(vec!["./entrypoint.sh", "bash"]),
-        host_config: Some(HostConfig{
+        host_config: Some(HostConfig {
             mounts: Some(mounts),
             shm_size: Some(1073741824),
             ..Default::default()
@@ -74,14 +80,16 @@ async fn create_container(docker: Docker) -> Result<ContainerCreateResponse, bol
         ..Default::default()
     };
 
-    match docker.create_container::<&str, &str>(None, alpine_config)
-        .await {
+    match docker
+        .create_container::<&str, &str>(None, alpine_config)
+        .await
+    {
         Ok(it) => return Ok(it),
         Err(err) => return Err(err),
     }
 }
 
-async fn remove_container(docker: Docker, id: String){
+async fn remove_container(docker: Docker, id: String) {
     let _ = docker
         .remove_container(
             &id,
@@ -93,7 +101,7 @@ async fn remove_container(docker: Docker, id: String){
         .await;
 }
 
-async fn exec(docker: Docker, id: &str) -> String{
+async fn exec(docker: Docker, id: &str) -> String {
     let exec = docker
         .create_exec(
             &id,
@@ -106,15 +114,16 @@ async fn exec(docker: Docker, id: &str) -> String{
         )
         .await;
 
-    
     let mut response: String = "".to_string();
-    if let StartExecResults::Attached { mut output, .. } = docker.start_exec(&exec.unwrap().id, None).await.unwrap() {
+    if let StartExecResults::Attached { mut output, .. } =
+        docker.start_exec(&exec.unwrap().id, None).await.unwrap()
+    {
         while let Some(Ok(msg)) = output.next().await {
             response = response + &msg.to_string();
         }
     } else {
         unreachable!();
-    }   
+    }
 
     response
 }
